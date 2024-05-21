3901ad5f9c5afe8e06398fdbe95f9f9c3d57107e
Radek Řeřicha <radek.rericha@gmail.com>
2024-05-21 13:24:45 +0200

    basic antd layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import type { Metadata } from "next";
+import { Layout } from "antd";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Content, Header } from "antd/es/layout/layout";
+import "./index.css"
+
 
 export const metadata: Metadata = {
   title: "Covid Data App",
@@ -10,9 +15,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>
+          <Layout style={{ display:"flex", minHeight:"100vh" }}>
+            <Header style={{ background: 'white' }}>
+              <strong>Covid Data App</strong>              
+            </Header>
+            <Content style={{ padding: '0 48px' }}>
+              {children}            
+            </Content>
+          </Layout>  
+        </AntdRegistry>    
+      </body>
     </html>
   );
 }
