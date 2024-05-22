f6ec7bc4c681833e5692b938a20df00dea80c08a
Radek Řeřicha <radek.rericha@gmail.com>
2024-05-22 15:03:55 +0200

    cors for verel

--- src/app/_components/variants.tsx
+++ src/app/_components/variants.tsx
@@ -10,11 +10,7 @@ export function Variants() {
   useEffect(() => {
     async function loadData() {
       const response = await fetch("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/COVID-19/geography_types/Nation/geographies/England/metrics/COVID-19_cases_lineagePercentByWeek?date=2024-01-01&page_size=365&year=2024",{
-        mode: 'no-cors',
-        method: "get",
-        headers: {
-             "Content-Type": "application/json"
-        }});
+        mode: 'no-cors'});
       const jsonData = await response.json();
       const cleanData = jsonData.results.map((r: { stratum: string, metric_value: number }) => {
         return { variant: r.stratum, percent: r.metric_value }
